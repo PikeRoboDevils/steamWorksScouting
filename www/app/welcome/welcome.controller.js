ecf1893c39acc2e79f5da96bd1224d1f634eda5c
Davis <derekdavis2013@gmail.com>
2017-01-14 16:31:46 -0500

    Add lodash

--- www/app/welcome/welcome.controller.js
+++ www/app/welcome/welcome.controller.js
@@ -8,5 +8,23 @@
 
 	function welcomeCtrl(){
 		var vm = this;
+
+		vm.matchProperties = {
+			matchNumber: null,
+			teamNumber: null
+		};
+
+		vm.isFormValid = isFormValid;
+		vm.submit = submit;
+
+		function isFormValid() {
+			var matchNumber = _.toString(vm.matchProperties.matchNumber),
+				teamNumber = _.toString(vm.matchProperties.teamNumber);
+			return !((matchNumber.length >= 2) && (teamNumber.length >= 2));
+		}
+
+		function submit() {
+			console.log(vm.matchProperties);
+		}
 	}
 })();
