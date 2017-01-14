84a25ea55e5e7c96fa253f3b5c4c1bff485c71af
Davis <derekdavis2013@gmail.com>
2017-01-14 14:29:10 -0500

    Create welcome controller

---
+++ www/app/welcome/welcome.controller.js
@@ -0,0 +1,12 @@
+(function() {
+	'use strict';
+	angular
+	.module('steamWorks')
+	.controller('welcomeCtrl', welcomeCtrl);
+
+	welcomeCtrl.$inject = [];
+
+	function welcomeCtrl(){
+		var vm = this;
+	}
+})();
