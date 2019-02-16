670a0e060079a863b5c86c4abdfbb2bab74da1c5
abril <abrilquebrado13@gmail.com>
2019-02-16 12:03:12 -0500

    finished UwU

--- www/app/auto/auto.controller.js
+++ www/app/auto/auto.controller.js
@@ -29,10 +29,6 @@
 				stationHatch: 0,
 				hatchDropped: 0,
 				cargoDropped: 0,
-				switch: 0,
-				cubeDropped: 0,
-				scale: 0,
-                exchange: 0,
 				autoRun: false,
 				placement: {
 					id: 0,
@@ -150,8 +146,7 @@
            
            
             vm.validStartingPos = validStartingPos;
-            vm.increaseExchange1 = increaseExchange1;
-            vm.decreaseExchange1 = decreaseExchange1;
+         
 
             init();
 
@@ -266,136 +261,6 @@
 				vm.matchProperties.cargoDropped += 1;
 			}
 
-			
-      function init() {
-        console.log(vm.match);
-      }
-
-      function decreaseHatch1() {
-        if(vm.matchProperties.hatch - 1 >= 0) {
-          vm.matchProperties.hatch -= 1;
-        }
-      }
-      function increaseHatch1() {
-        vm.matchProperties.hatch += 1;
-      }
-
-      function decreaseRocketHatchOne1() {
-        if(vm.matchProperties.rocketHatchOne - 1 >= 0) {
-          vm.matchProperties.rocketHatchOne -= 1;
-        }
-      }
-      function decreaseRocketHatchTwo1() {
-        if(vm.matchProperties.rocketHatchTwo - 1 >= 0) {
-          vm.matchProperties.rocketHatchTwo -= 1;
-        }
-      }
-      function decreaseRocketHatchThree1() {
-        if(vm.matchProperties.rocketHatchThree - 1 >= 0) {
-          vm.matchProperties.rocketHatchThree -= 1;
-        }
-      }
-      function increaseRocketHatchOne1() {
-        vm.matchProperties.rocketHatchOne += 1;
-      }
-      function increaseRocketHatchTwo1() {
-        vm.matchProperties.rocketHatchTwo += 1;
-      }
-      function increaseRocketHatchThree1() {
-        vm.matchProperties.rocketHatchThree += 1;
-      }
-      function decreaseStationHatch1() {
-        if(vm.matchProperties.stationHatch - 1 >= 0) {
-          vm.matchProperties.stationHatch -= 1;
-        }
-      }
-      function increaseStationHatch1() {
-        vm.matchProperties.stationHatch += 1;
-      }
-
-
-      function decreaseHatchDropped1() {
-        if(vm.matchProperties.hatchDropped - 1 >= 0) {
-          vm.matchProperties.hatchDropped -= 1;
-        }
-      }
-
-      function increaseHatchDropped1() {
-        vm.matchProperties.hatchDropped += 1;
-      }
-
-
-      
-    
-      function increaseCargo1() {
-        vm.matchProperties.cargo += 1;
-      }
-      function decreaseCargo1() {
-        if(vm.matchProperties.cargo - 1 >= 0) {
-          vm.matchProperties.cargo -= 1;
-        }
-      }
-
-      function increaseRocketCargoOne1() {
-        vm.matchProperties.rocketCargoOne += 1;
-      }
-      function increaseRocketCargoTwo1() {
-        vm.matchProperties.rocketCargoTwo += 1;
-      }
-      function increaseRocketCargoThree1() {
-        vm.matchProperties.rocketCargoThree += 1;
-      }
-
-      function decreaseRocketCargoOne1() {
-        if(vm.matchProperties.rocketCargoOne - 1 >= 0) {
-          vm.matchProperties.rocketCargoOne -= 1;
-        }
-      }
-      function decreaseRocketCargoTwo1() {
-        if(vm.matchProperties.rocketCargoTwo - 1 >= 0) {
-          vm.matchProperties.rocketCargoTwo -= 1;
-        }
-      }
-      function decreaseRocketCargoThree1() {
-        if(vm.matchProperties.rocketCargoThree - 1 >= 0) {
-          vm.matchProperties.rocketCargoThree -= 1;
-        }
-      }
-
-      function increaseStationCargo1() {
-        vm.matchProperties.stationCargo += 1;
-      }
-      function decreaseStationCargo1() {
-        if(vm.matchProperties.stationCargo - 1 >= 0) {
-          vm.matchProperties.stationCargo -= 1;
-        }
-      }
-
-      function decreaseCargoDropped1() {
-        if(vm.matchProperties.cargoDropped - 1 >= 0) {
-          vm.matchProperties.cargoDropped -= 1;
-        }
-      }
-
-      function increaseCargoDropped1() {
-        vm.matchProperties.cargoDropped += 1;
-      }
-            function decreaseExchange1() {
-        if(vm.matchProperties.exchange - 1 >= 0) {
-          vm.matchProperties.exchange-= 1;
-        }
-                
-                
-      }
-            function increaseExchange1() {
-        vm.matchProperties.exchange += 1;
-      }
-    
-          
-
-           
-            
-
     
 
       function validStartingPos() {
@@ -412,39 +277,21 @@
 
       function submit() {
         var autoScore = {
-                    //fuelpoints
-					switchPoints: vm.matchProperties.switch,
-					hatchPoints: vm.matchProperties.hatch,
-					rocketHatchTwoPoints: vm.matchProperties.rocketHatchTwo,
-					rocketHatchOnePoints: vm.matchProperties.rocketHatchOne,
-					rocketHatchThreePoints: vm.matchProperties.rocketHatchThree,
-					stationHatchPoints: vm.matchProperties.stationHatch,
-                    //rotorpoints
-					scalePoints: vm.matchProperties.scale,
-					cargoPoints: vm.matchProperties.cargo,
-					rocketCargoTwoPoints: vm.matchProperties.rocketCargoTwo,
-					rocketCargoOnePoints: vm.matchProperties.rocketCargoOne,
-					rocketCargoThreePoints: vm.matchProperties.rocketCargoThree,
-					stationCargoPoints: vm.matchProperties.stationCargo,
-
-          switchPoints: vm.matchProperties.switch,
+                   
           hatchPoints: vm.matchProperties.hatch,
           rocketHatchTwoPoints: vm.matchProperties.rocketHatchTwo,
           rocketHatchOnePoints: vm.matchProperties.rocketHatchOne,
           rocketHatchThreePoints: vm.matchProperties.rocketHatchThree,
           stationHatchPoints: vm.matchProperties.stationHatch,
-                    //rotorpoints
-          scalePoints: vm.matchProperties.scale,
+         
           cargoPoints: vm.matchProperties.cargo,
           rocketCargoTwoPoints: vm.matchProperties.rocketCargoTwo,
           rocketCargoOnePoints: vm.matchProperties.rocketCargoOne,
           rocketCargoThreePoints: vm.matchProperties.rocketCargoThree,
           stationCargoPoints: vm.matchProperties.stationCargo,
 
-                    //basepoints
-          exchangePoints: vm.matchProperties.exchange,
-                    hatchesDropped: vm.matchProperties.hatchDropped,
-                    cargoesDropped: vm.matchProperties.cargoDropped,
+          hatchesDropped: vm.matchProperties.hatchDropped,
+          cargoesDropped: vm.matchProperties.cargoDropped,
           total: 0,
           autoRunPoints: vm.matchProperties.autoRun,
           placement: vm.matchProperties.placement.value,
@@ -457,20 +304,14 @@
         autoScore.autoRunPoints = vm.matchProperties.autoRun ? MatchSvc.constants.AUTOLINE_CONSTANT : 0;
       
         autoScore.total = autoScore.autoRunPoints;
-            
-        autoScore.cubes = autoScore.switchPoints + autoScore.scalePoints + autoScore.exchangePoints + autoScore.dropped;
 
         autoScore.cargo = (autoScore.rocketCargoOnePoints + autoScore.rocketCargoTwoPoints + autoScore.rocketCargoThreePoints + autoScore.stationCargoPoints);
 
-       autoScore.hatch = (autoScore.rocketHatchOnePoints + autoScore.rocketHatchTwoPoints + autoScore.rocketHatchThreePoints+ autoScore.stationHatchPoints);
+       	autoScore.hatch = (autoScore.rocketHatchOnePoints + autoScore.rocketHatchTwoPoints + autoScore.rocketHatchThreePoints+ autoScore.stationHatchPoints);
 
+        autoScore.hatchDropped = autoScore.hatchesDropped;
 
-                autoScore.hatch = (autoScore.rocketHatchOnePoints + autoScore.rocketHatchTwoPoints + autoScore.rocketHatchThreePoints+ autoScore.stationHatchPoints);
-
-                autoScore.hatchDropped = autoScore.hatchesDropped;
-
-
-                autoScore.cargoDropped = autoScore.cargoesDropped;
+		autoScore.cargoDropped = autoScore.cargoesDropped;
 
         vm.match.autoScore = autoScore;
         MatchSvc.updateMatch(vm.match);
--- www/app/teleOp/teleOp.controller.js
+++ www/app/teleOp/teleOp.controller.js
@@ -30,18 +30,8 @@
 			stationHatch: 0,
 			loadingCargo: 0,
 			loadingHatch: 0,
-			switchCube: 0,
-            cubeDropped: 0,
-			scaleCube: 0,
             total: 0,
-            vaultPoints: 0,
             climbPoints: 0,
-            parking: 0,
-            exchangeCube: 0,
-            vaultCube: 0,
-            outPortal: 0,
-            levitatePoints: 0,
-            outExchange: 0,
             breakdown: false,
 			climbSuccess: false,
 			defending: false,
@@ -57,17 +47,6 @@
 				label: 'Offensive',
 				value: 'OFFENSIVE'
 				},
-            powerUp: {
-                id: 2,
-                label: 'None',
-                value: 'NONE'
-            },
-
-            foul: {
-            	id: 5,
-                label: 'None',
-                value: 'NONE'
-            },
 
             rocketLevel: {
             	id: 1,
@@ -146,34 +125,7 @@
 				value: 'DEFENSIVE'
 			}
 		];
-		vm.fouls = [
-			{
-				id: 1,
-				label: 'Foul',
-				value: 'FOUL'
-			},
-			{
-				id: 2,
-				label: 'Technical Foul',
-				value: 'TECH'
-			},
-			{
-				id: 3,
-				label: 'Yellow Card',
-				value: 'YELLOW CARD'
-			},
-			{
-				id: 4,
-				label: 'Red Card',
-				value: 'RED CARD'
-			},
-			{
-				id: 5,
-				label: 'None',
-				value: 'NONE'
-			}
-		];
-
+		
 
 			vm.increaseRocketCargoOne1 = increaseRocketCargoOne1;
 			vm.decreaseRocketCargoOne1 = decreaseRocketCargoOne1;
@@ -202,28 +154,10 @@
 			vm.increaseCargoDropped1 = increaseCargoDropped1;
 			vm.decreaseCargoDropped1 = decreaseCargoDropped1;
 
-
-
- 			vm.increaseSwitchCube1 = increaseSwitchCube1;
-
-			vm.decreaseSwitchCube1 = decreaseSwitchCube1;
-			vm.increaseScaleCube1 = increaseScaleCube1;
-			vm.decreaseScaleCube1 = decreaseScaleCube1;
 			vm.toggleclimbSuccess = toggleclimbSuccess;
-            vm.decreaseCubeDropped1 = decreaseCubeDropped1;
-            vm.increaseCubeDropped1 = increaseCubeDropped1;
-			vm.increaseExchangeCube1 = increaseExchangeCube1;
-			vm.decreaseExchangeCube1 = decreaseExchangeCube1;
-			vm.increaseVaultCube1 = increaseVaultCube1;
-            vm.decreaseVaultCube1 = decreaseVaultCube1;
-            vm.increaseOutPortal1 = increaseOutPortal1;
-            vm.decreaseOutPortal1 = decreaseOutPortal1;
-            vm.increaseOutExchange1 = increaseOutExchange1;
-            vm.decreaseOutExchange1 = decreaseOutExchange1;
             vm.toggleClimbAttempt = toggleClimbAttempt;
             vm.toggleDefending = toggleDefending;
             vm.toggleBreakdown = toggleBreakdown;
-            vm.toggleLevitatePoints = toggleLevitatePoints;
             vm.didClimb = didClimb;
             vm.validClimbPos = validClimbPos;
 
@@ -363,81 +297,6 @@
 			}
 
 
-			function decreaseSwitchCube1() {
-				if(vm.matchParts.switchCube - 1 >= 0) {
-					vm.matchParts.switchCube -= 1;
-				}
-			}
-        
-            	function decreaseCubeDropped1() {
-				if(vm.matchParts.cubeDropped - 1 >= 0) {
-					vm.matchParts.cubeDropped -= 1;
-				}
-			}
-        
-            function increaseCubeDropped1() {
-				vm.matchParts.cubeDropped += 1;
-			}
-        
-
-			function increaseSwitchCube1() {
-				vm.matchParts.switchCube += 1;
-			}
-        
-        
-            function decreaseExchangeCube1() {
-				if(vm.matchParts.exchangeCube - 1 >= 0) {
-					vm.matchParts.exchangeCube -= 1;
-				}
-			}
-        
-            function increaseExchangeCube1() {
-				vm.matchParts.exchangeCube += 1;
-			}
-            
-        //makes sure that user can't exceed past 9 cubes delivered to the vault
-			function increaseVaultCube1() {
-                if (vm.matchParts.vaultCube - 8 <= 0) {
-				vm.matchParts.vaultCube += 1;
-                }
-			}
-        
-            function decreaseVaultCube1() {
-				if(vm.matchParts.vaultCube - 1 >= 0) {
-					vm.matchParts.vaultCube -= 1;
-				}
-			}
-
-			function decreaseScaleCube1() {
-				if(vm.matchParts.scaleCube - 1 >= 0) {
-					vm.matchParts.scaleCube -= 1;
-				}
-			}
-
-			function increaseScaleCube1() {
-				vm.matchParts.scaleCube += 1;
-			}
-
-			function increaseOutPortal1() {
-				vm.matchParts.outPortal += 1;
-			}
-
-			function decreaseOutPortal1() {
-				if(vm.matchParts.outPortal - 1 >= 0) {
-					vm.matchParts.outPortal -= 1;
-				}
-			}
-
-			function increaseOutExchange1() {
-				vm.matchParts.outExchange += 1;
-			}
-
-			function decreaseOutExchange1() {
-				if(vm.matchParts.outExchange - 1 >= 0) {
-					vm.matchParts.outExchange -= 1;
-				}
-			}
-
             function hasClimbed(){
             	return vm.matchParts.climbSuccess;
             }
@@ -466,24 +325,21 @@
 
 			function submit() {
 				var teleScore = {
-					vaultPoints: vm.matchParts.vaultPoints,
 					climbPoints: vm.matchParts.climbPoints,
 					total: vm.matchParts.total,
-					parking: vm.matchParts.parking,
 					climbSuccess: vm.matchParts.climbSuccess,
 					climbAttempt: vm.matchParts.climbAttempt,
 					defending: vm.matchParts.defending,
 					playStyle: vm.matchParts.playStyle.value,
 					rocketLevel: vm.matchParts.rocketLevel.value,
 					climbLevel: vm.matchParts.climbLevel.value,
-                    foul: vm.matchParts.foul.value,
                     breakdown: vm.matchParts.breakdown,
-                    rocketCargoOnePoints: vm.matchParts.rocketCargoOnePoints,
-                    rocketCargoTwoPoints: vm.matchParts.rocketCargoTwoPoints,
-                    rocketCargoThreePoints: vm.matchParts.rocketCargoThreePoints,
-                    rocketHatchOnePoints: vm.matchParts.rocketHatchOnePoints,
-                    rocketHatchTwoPoints: vm.matchParts.rocketHatchTwoPoints,
-                    rocketHatchThreePoints: vm.matchParts.rocketHatchThreePoints,
+                    rocketCargoOnePoints: vm.matchParts.rocketCargoOne,
+                    rocketCargoTwoPoints: vm.matchParts.rocketCargoTwo,
+                    rocketCargoThreePoints: vm.matchParts.rocketCargoThree,
+                    rocketHatchOnePoints: vm.matchParts.rocketHatchOne,
+                    rocketHatchTwoPoints: vm.matchParts.rocketHatchTwo,
+                    rocketHatchThreePoints: vm.matchParts.rocketHatchThree,
                     stationCargo: vm.matchParts.stationCargo,
                     stationHatch: vm.matchParts.stationHatch,
                     loadingCargo: vm.matchParts.loadingCargo,
@@ -491,49 +347,22 @@
                    	hatchesDropped: vm.matchParts.hatchDropped,
                    	cargoesDropped: vm.matchParts.cargoDropped,
 
-                    //powerUp: vm.matchParts.powerUp.value,
-                    switchCube: vm.matchParts.switchCube,
-                    scaleCube: vm.matchParts.scaleCube,
-                    exchangeCube: vm.matchParts.exchangeCube,
-                    vaultCube: vm.matchParts.vaultCube,
-                    outExchange: vm.matchParts.outExchange,
-                    dropped: vm.matchParts.cubeDropped,
-                    outPortal: vm.matchParts.outPortal,
-                    levitation: vm.matchParts.levitatePoints
 				};
 
-               
-                teleScore.vaultPoints = (vm.matchParts.vaultCube * MatchSvc.constants.TELE_VAULT_CUBE_CONSTANT);
                 
 				teleScore.climbPoints += vm.matchParts.climbSuccess ? MatchSvc.constants.CLIMB_CONSTANT : 0;
-                
-				teleScore.parking = (vm.matchParts.parking * MatchSvc.constants.TELE_PARKING_CONSTANT);
-                
-                teleScore.levitation = vm.matchParts.levitatePoints;
-
-				teleScore.total = teleScore.vaultPoints + teleScore.climbPoints + teleScore.parking + teleScore.levitation;
 
                 teleScore.cargo = (teleScore.stationCargo + teleScore.rocketCargoOnePoints + teleScore.rocketCargoTwoPoints + teleScore.rocketCargoThreePoints);
 
                 teleScore.hatch = (teleScore.stationHatch + teleScore.rocketHatchOnePoints + teleScore.rocketHatchTwoPoints + teleScore.rocketHatchThreePoints);
-
-                teleScore.fouls = teleScore.foul;
                 
-                teleScore.cubes = (teleScore.switchCube + teleScore.scaleCube);
-                
-                teleScore.extraCubes = teleScore.exchangeCube + teleScore.vaultCube;
-                
-                 teleScore.cargoDropped = teleScore.cargoesDropped;
+                teleScore.cargoDropped = teleScore.cargoesDropped;
                 teleScore.hatchDropped = teleScore.hatchesDropped;
-
-                //these cubes are left over, maybe put in another category?: teleScore.outPortal + teleScore.outExchange
 
 				vm.match.teleScore = teleScore;
 				MatchSvc.updateMatch(vm.match);
                 console.log(vm.match.teleScore);
 				$state.go('results');
-               // console.log(teleScore.total);
-               // console.log("telescore vault points: " + teleScore.vaultPoints);
                 
 			}
 
@@ -545,11 +374,6 @@
 			function toggleClimbAttempt(){
 				vm.matchParts.climbAttempt = !vm.matchParts.climbAttempt;
 			}
-        
-            function toggleLevitatePoints(){
-                vm.matchParts.levitatePoints = 30;
-                
-            }
         
             function toggleBreakdown(){
                 vm.matchParts.breakdown = !vm.matchParts.breakdown;
